2bc346b9df4d16cfcf89fae598f9bb196c19bf82
Nauro <nauro@outlook.com>
2019-02-07 00:37:03 -0200

    Status atual do site

---
+++ index.php
@@ -0,0 +1,82 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Sandbox do Nauro</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Home</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home só que do lado de cá
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="mt-5">Nauro's Sandbox</h1>
+        <p class="lead">É só um sandbox pra mim... Se você caiu aqui por curiosidade, é um engano, não tem nada, vá embora... xô... ¯\_(ツ)_/¯</p>
+      </div>
+    </div>
+  <div class="row">
+      <div class="col-lg-12 text-center">
+          <p><i>sand·box: "A shallow box or hollow in the ground partly filled with sand for children to play in."</i></p>
+          <p><img src="https://i.pinimg.com/originals/6b/b7/ed/6bb7ed87e2f041324a2d2e8c6fc70c8d.jpg" /></p>
+      </div>
+  </div>
+  <div class="row">
+      <div class="col-lg-12">
+        <p>Tracking do que eu mexo só pra eu não me perder:</p>
+	<ul>
+            <li>07/02/2019.5 - Adicionando link do Facebook pra caso um alienígena me abduza e eu esqueça quem eu sou... Ou só pra passar vergonha no futuro quando eu acessar isso daqui mesmo...</li>
+            <li>07/02/2019.4 - Instalando PHPzão da Massa™, MySQL, Node, Npm, Composer, Git, Docker e essas coisa aí</li>
+            <li>07/02/2019.3 - Chorando enquanto desliza pela parede por isso não tá no github e sendo feito deploy automático ainda.</li>
+            <li>07/02/2019.2 - Adicionando texto do sandbox e imagem</li>
+            <li>07/02/2019.1 - Adicionando SSL e forçando https</li>
+            <li>06/02/2019 - Adicionando esse template qualquer</li> 
+            <li>05/02/2019 - Criando servidor pra fazer uns testes</li>
+       </ul>
+      </div>
+    </div>
+    <div class="col-lg-12">
+        <a target="_blank" href="https://www.facebook.com/naurojunior"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-32.png" /></a>
+    </div>
+  </div>
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
+
