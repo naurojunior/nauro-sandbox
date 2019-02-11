082b124b46c6666099778194670a007bdfd7fc3c
Nauro <nauro@outlook.com>
2019-02-11 01:08:04 -0200

    Adição de "Why" e "Changelog"

--- src/Changelog.js
+++ src/Changelog.js
@@ -1,45 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
 
 class Changelog extends Component {
 	render(){
 		return(<div>
-		        <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-		          <div className="container">
-		            <Link to="/" className="navbar-brand">Home</Link>
-		            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-		              <span className="navbar-toggler-icon"></span>
-		            </button>
-		            <div className="collapse navbar-collapse" id="navbarResponsive">
-		              <ul className="navbar-nav ml-auto">
-		                <li className="nav-item active">
-		                  <Link to="/why" className="nav-link">Por que?
-		                    <span className="sr-only">(current)</span>
-		                  </Link>
-		                </li>
-		              </ul>
-		            </div>
-		          </div>
-		        </nav>
+		        <Navbar />
 
 		        <div className="container">
 		          <div className="row">
 		            <div className="col-lg-12 text-center">
 		              <h1 className="mt-5">Nauro's Sandbox</h1>
-		              <p className="lead">Por que criar um site assim?</p>
+		              <p className="lead">Changelog das PARADA</p>
 		            </div>
 		          </div>
-		        <div className="row">
-		            <div className="col-lg-12 text-center">
-		                <p><i>sand·box: "A shallow box or hollow in the ground partly filled with sand for children to play in."</i></p>
-		                <p><img className="img-fluid" src="https://i.pinimg.com/originals/6b/b7/ed/6bb7ed87e2f041324a2d2e8c6fc70c8d.jpg" /></p>
-		            </div>
-		        </div>
 		        <div className="row">
 		            <div className="col-lg-12">
 		              <p>Tracking do que eu mexo só pra eu não me perder:</p>
 		               <ul>
-						          <li>11/02/2019 - Adicionando Analytics</li>
+						  <li>11/02/2019.3 - Adicionando "Por Que?" e "Changelog"</li>
+						  <li>11/02/2019.2 - Adicionando rotas</li>
+		                  <li>11/02/2019.1 - Adicionando Analytics</li>
 		                  <li>08/02/2019.2 - Adicionando miniscript de deploy</li>
 		                  <li>08/02/2019.1 - Convertendo pra ReactJS. Por enquanto o código é porco, mas pelo menos é react ¯\_(ツ)_/¯</li>
 		                  <li>07/02/2019.6 - Adicionando esse código no git pra eu não pensar em tacar fogo no PC toda vez que edito ele. Clonagem ainda tá manual, porque o mundo não foi feito em um dia</li>
---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+	render(){
+		return(<nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+		          <div className="container">
+		            <Link to="/" className="navbar-brand">Home</Link>
+		            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+		              <span className="navbar-toggler-icon"></span>
+		            </button>
+		            <div className="collapse navbar-collapse" id="navbarResponsive">
+		              <ul className="navbar-nav ml-auto">
+		                <li className="nav-item active">
+		                  <Link to="/why" className="nav-link">Por que?
+		                    <span className="sr-only">(current)</span>
+		                  </Link>
+		                </li>
+		                <li className="nav-item active">
+		                  <Link to="/changelog" className="nav-link">Changelog
+		                    <span className="sr-only">(current)</span>
+		                  </Link>
+		                </li>
+		              </ul>
+		            </div>
+		          </div>
+		        </nav>);
+		}
+}
+
+
+export default Navbar;
